02aa9779467b132055f0b97e805cadac29f7a1fb
Yaşar ÖKTEN <yasaroktnn@gmail.com>
2023-03-15 17:54:41 +0300

    fix

--- level3/add_prime_sum.c
+++ level3/add_prime_sum.c
@@ -1,52 +1,52 @@
 #include <unistd.h>
 
-int	ft_atoi(char *str)
+void ft_putnbr(int nbr)
 {
-	int i = 0;
-	int res = 0;
-	while(str[i])
-		res = (res * 10) + str[i++] - '0';
-	return(res);
+    char *res = "0123456789";
+    if (nbr > 9)
+        ft_putnbr(nbr / 10);
+    write(1, &res[nbr % 10], 1);
 }
 
-void	ft_putnbr(int nbr)
+int ft_atoi (char *str)
 {
-	char res[] = "0123456789";
-	if(nbr >= 10)
-		ft_putnbr(nbr / 10);
-	write(1, &res[nbr % 10], 1);
+    int i = 0;
+    int res;
+    while (str[i] && str[i] >= '0' && str[i] <= '9')
+        res = (res * 10) + str[i++] - '0';
+    return (res);
 }
 
-int prime_number_detection(int nbr)
+int prime_nbr(int nbr)
 {
-	int i = 2;
-	if ((nbr == 1) || (nbr == 0))
-		return(0);
-	while(i < nbr)
-	{
-		if(nbr % i == 0)
-			return(0);
-		i++;
-	}
-	return(1);
+    int i = 2;
+    if (nbr == 1)
+        return (0);
+    while (i < nbr)
+    {
+        if (nbr % i == 0)
+            return (0);
+        i++;
+    }
+    return (1);
 }
 
-int main (int ac, char **av)
+int main(int ac, char **av)
 {
-	if(ac == 2)
-	{
-		int res = 0;
-		int i = 0;
-		int nbr = ft_atoi(av[1]);
-		while(i <= nbr)
-		{
-			if(prime_number_detection(i) == 1)
-				res += i;
-			i++;
-		}
-		ft_putnbr(res);
-		write(1, "\n", 1);
-	}
-	else
-		write(1, "0\n", 2);
-}
+    if (ac != 2)
+        write(1, "0", 1);
+    if (ac == 2)
+    {
+        int i = 1;
+        int res = 0;
+        int av1 = ft_atoi(av[1]);
+        while (i <= av1)
+        {
+            if (prime_nbr(i) == 1)
+                res += i;
+            i++;
+        }
+        ft_putnbr(res);
+    }
+    write(1, "\n", 1);
+}  
--- level4/rostring.c
+++ level4/rostring.c
@@ -1,59 +1,47 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rostring.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yokten <yokten@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/02 19:13:44 by yokten            #+#    #+#             */
-/*   Updated: 2023/03/02 20:44:32 by yokten           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-int ft_strlen(char *str)
+int ft_strlen (char *str)
 {
-	int i = 0;
-	while(str[i])
-		i++;
-	return(i);
+    int i = 0;
+    while(str[i])
+        i++;
+    return(i);
 }
 
 int main(int ac, char **av)
 {
-	if(ac >= 2)
-	{
-		int i = 0;
-		while(av[1][i] && (av[1][i] == ' '  || av[1][i] == '\t'))
-			i++;
-		int j = i;
-		while(av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
-			i++;
-		int k = i;
-		while(av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
-			i++;
-		int len = ft_strlen(av[1]) - 1;
-		while(av[1][len] &&(av[1][len] == ' ' || av[1][len] == '\t'))
-			len--;
-		while(av[1][i] && i <= len)
-		{
-			if(av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
-			{
-				while(av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
-					i++;
-				write(1, " ", 1);
-			}
-			if(av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
-				write(1, &av[1][i++], 1);
-		}
-		while(av[1][k] && (av[1][k] == ' ' || av[1][k] == '\t'))
-			k++;
-		if (av[1][j]  && (av[1][j - 1] == ' ' || av[1][j - 1] == '\t' || !av[1][j - 1]))
-			if(av[1][k] && (av[1][k] != ' ' && av[1][k] != '\t'))
-				write(1, " ", 1);
-		while (av[1][j] && (av[1][j] != ' ' && av[1][j] != '\t'))
-			write(1, &av[1][j++], 1);
-	}
-	write(1, "\n", 1);
+    if (ac >= 2)
+    {
+        int i = 0;
+        int len = ft_strlen(av[1]);
+
+        while (av[1][len] == ' ' || av[1][len] == '\t')
+            len --;
+        while (av[1][i] == ' ' || av[1][i] == '\t')
+            i++;
+        int j = i;
+        while ((av[1][i] != ' ' && av[1][i] != '\t') && av[1][i])
+            i++;
+        int k = i;
+        while ((av[1][i] == ' ' || av[1][i] == '\t') && av[1][i])
+            i++;
+        while (av[1][i] && i <= len)
+        {
+            if (av[1][i] == ' ' ||   av[1][i] == '\t')
+            {
+                while (av[1][i] == ' ' || av[1][i] == '\t')
+                    i++;
+                if (av[1][i] != '\0')
+                    write(1, " ", 1);
+            }
+            write(1, &av[1][i++], 1);
+        }
+        while (av[1][k] && (av[1][k] == ' ' || av[1][k] == '\t'))
+            k++;
+        if (av[1][k] && (av[1][k] != ' ' && av[1][k] != '\t'))
+            write(1, " ", 1);
+        while (av[1][j] && (av[1][j] != ' ' && av[1][j] != '\t'))
+            write(1, &av[1][j++], 1);
+    }
+        write(1, "\n", 1);
 }
