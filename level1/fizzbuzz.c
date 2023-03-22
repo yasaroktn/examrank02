2a50a341dc30135d292be33d7fa388d1ccd09356
Yaşar ÖKTEN <yasaroktnn@gmail.com>
2023-03-23 00:28:10 +0300

    fix

--- level1/fizzbuzz.c
+++ level1/fizzbuzz.c
@@ -1,40 +1,27 @@
 #include <unistd.h>
 
-void    ft_putnbr(int nbr)
+void ft_putnbr(char nbr)
 {
-    char *res;
-    if(nbr > 9)
+    char *res = "0123456789";
+    if (nbr >= 10)
         ft_putnbr(nbr / 10);
-
-    res = nbr % 10 + '0';
-    write(1, &res, 1);
+    write (1, &res[nbr % 10], 1);
 }
 
 int main(int ac, char **av)
 {
-    int i = 1;
-    while(i <= 100)
+    int nbr = 1;
+    while (nbr <= 100)
     {
-        if(i % 3 == 0 && i % 5 == 0)
-        {
-            write(1, "fizzbuzz\n", 9);
-            i++;
-        }
-        else if(i % 3 == 0)
-        {
-            write(1, "fizz\n", 5);
-            i++;
-        }
-        else if(i % 5 == 0)
-        {
-            write(1, "buzz\n", 5);
-            i++;
-        }
+        if (nbr % 3 == 0 && nbr % 5 == 0)
+            write (1, "fizzbuzz", 8);
+        else if (nbr % 3 == 0)
+            write (1, "fizz", 4);
+        else if (nbr % 5 == 0)
+            write (1, "buzz", 4);
         else
-        {
-            ft_putnbr(i);
-            write(1, "\n", 1);
-            i++;
-        }
+            ft_putnbr(nbr);
+        write (1, "\n", 1);
+        nbr ++;
     }
 }
